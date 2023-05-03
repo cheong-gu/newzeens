8d7a5d45f16d5e011f881406d54cdad581945064
Yoonable <yyw241@gmail.com>
2023-05-03 13:19:41 +0900

    :lipstick: 헤더 CSS 추가

---
+++ app/components/banners.tsx
@@ -0,0 +1,9 @@
+import styled from "@emotion/styled";
+
+export default function Banners() {
+  const BannerStyle = styled.div`
+    height: 198px;
+    background-color: blue;
+  `;
+  return <BannerStyle />;
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,16 @@
+"use client";
+
+import styled from "@emotion/styled";
+import Banners from "./components/banners";
+
 export default function Home() {
-  return <div>home</div>;
+  const ContainerStyle = styled.div``;
+
+  const FilterStyle = styled.div``;
+  return (
+    <ContainerStyle>
+      <Banners></Banners>
+      <FilterStyle></FilterStyle>
+    </ContainerStyle>
+  );
 }
