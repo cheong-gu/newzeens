4a7de21d754de86809eec0ededfd01f4c181f591
gparkkii <jyp933@gmail.com>
2023-06-01 20:09:57 +0900

    메인페이지 상세페이지 라우트 추가

--- app/components/newsLetter.tsx
+++ app/components/newsLetter.tsx
@@ -2,28 +2,37 @@ import styled from "@emotion/styled";
 import { ListProps } from "./newsLetters";
 import Image from "next/image";
 import { ImageStyle, OutLine } from "./styles/NewsLetter.styles";
+import Link from "next/link";
+import { NewsletterResponseType } from "../register/newsletter.type";
 
-export const NewsLetter = ({ list }: { list: ListProps | null }) => {
+export const NewsLetter = ({
+  list,
+}: {
+  list: NewsletterResponseType | null;
+}) => {
+  const encodedURI = encodeURIComponent(JSON.stringify(list));
   return (
     <OutLine>
-      <div className="grid">
-        <ImageStyle>
-          <Image className="image" src="/body.png" alt="image" fill />
-        </ImageStyle>
-        <ul>
-          <li>{list?.field}</li>
-          {/* {list?.keywords.map((el, idx) => (
+      <Link href={`detail/${list?._id}/${encodedURI}`}>
+        <div className="grid">
+          <ImageStyle>
+            <Image className="image" src="/body.png" alt="image" fill />
+          </ImageStyle>
+          <ul>
+            <li>{list?.field}</li>
+            {/* {list?.keywords.map((el, idx) => (
             <li key={idx}>{el}</li>
           ))} */}
-        </ul>
+          </ul>
 
-        <div>
-          <span className="title">{list?.newsletterName}</span>
-          <span className="publisher">{list?.publisher}</span>
+          <div>
+            <span className="title">{list?.newsletterName}</span>
+            <span className="publisher">{list?.publisher}</span>
+          </div>
+
+          <div className="introduction">{list?.introduction}</div>
         </div>
-
-        <div className="introduction">{list?.introduction}</div>
-      </div>
+      </Link>
     </OutLine>
   );
 };
