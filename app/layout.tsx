fdeea93b9772bae1f42926b793089d9591ef9473
gparkkii <jyp933@gmail.com>
2023-07-24 00:09:38 +0900

    파비콘 경로 수정

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
     <html lang="ko">
       <head>
         <title>NEWZEENS</title>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
+        <link rel="icon" href="static/public/favicon.ico" sizes="any" />
         <link
           rel="stylesheet"
           type="text/css"
